d4ca998f8f590858b61ee53284725525288f02e8
AlecCali <53955694+AlecCali@users.noreply.github.com>
2019-11-14 00:32:30 -0500

    Add files via upload

--- counting.py
+++ counting.py
@@ -1,16 +1,14 @@
 import urllib3
 from urllib.parse import urlparse
 
-govdomains = open("govdomains.txt", "r")
-print(govdomains.read().split('\n'))
-gd = govdomains.read().split('\n')
-print(gd)
+f = open('govdomains.txt', 'r')
+govdomains = f.read().split()
+print(govdomains)
 
 news = ["nytimes.com", "washingtonpost.com", "wsj.com", "foxbusiness.com", "foxnews.com", "pbs.org", "npr.org", "cnn.com", "nbcnews.com", "msnbc.com", "abcnews.go.com", "cbsnews.com", "latimes.com", "newsweek.com", "time.com", "bloomberg.com", "reuters.com", "huffpost.com", "newyorker.com", "usatoday.com", "slate.com", "buzzfeed.com", "foreignpolicy.com"]
 ngolst = ["clintonfoundation.org", "amnesty.org", "hrw.org", "oxfam.org", "ned.org", "freedomhouse.org", "opensocietyfoundations.org", "cfr.org", "ndi.org", "mercycorps.org", "mei.org", "worldjusticeproject.org", "usip.org"]
 cia = ["cia.gov"]
-gov = ["usaid.gov", "voanews.com", "alhuraa.com", "rfa.org", "rferl.org", "radiotelevisionmarti.com", "radiofarda.com"]
-allgov = gov + gd
+gov = ["usaid.gov", "voanews.com", "alhuraa.com", "rfa.org", "rferl.org", "radiotelevisionmarti.com", "radiofarda.com", "state.gov", "president.gov", "usgs.gov"]
 result = dict()
 
 URLs = open("refs/Afghanistan", "r")
@@ -18,7 +16,7 @@ for line in URLs.readlines():
     source = urlparse(line)
     if source.netloc in news:
         if source.netloc in result:
-            result[source.netloc] += result[source.netloc]
+            result[source.netloc] = result[source.netloc] + 1
         else:
             result.update({source.netloc : 1})
 print(result)
@@ -28,27 +26,27 @@ for line in URLs.readlines():
     source = urlparse(line)
     if source.netloc in ngolst:
         if source.netloc in result:
-            result[source.netloc] += result[source.netloc]
+            result[source.netloc] = result[source.netloc] + 1
         else:
-            result.update({source.netloc : 1})
-print(result)
+           result.update({source.netloc : 1})
+           print(result)
+
+#URLs =open("refs/Bolivia", "r")
+#for line in URLs.readlines():
+#    source = urlparse(line)
+#    if source.netloc in cia:
+#        if source.netloc in result:
+#            result[source.netloc] = result[source.netloc] + 1
+#        else:
+#           result.update({source.netloc : 1})
+#print(result)
 
 URLs =open("refs/Afghanistan", "r")
 for line in URLs.readlines():
     source = urlparse(line)
-    if source.netloc in cia:
+    if source.netloc in govdomains:
         if source.netloc in result:
-            result[source.netloc] += result[source.netloc]
-        else:
-            result.update({source.netloc : 1})
-print(result)
-
-URLs =open("refs/Afghanistan", "r")
-for line in URLs.readlines():
-    source = urlparse(line)
-    if source.netloc in allgov:
-        if source.netloc in result:
-            result[source.netloc] += result[source.netloc]
+            result[source.netloc] = result[source.netloc] + 1
         else:
             result.update({source.netloc : 1})
 print(result)
